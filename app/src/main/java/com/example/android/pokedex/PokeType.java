b3e5dd8eb149177b6d968db697e2da03d87ca636
smoisset <smoisset@worldremit.com>
2017-12-18 23:36:12 +0000

    Create PokeType and Sprite Class

---
+++ app/src/main/java/com/example/android/pokedex/PokeType.java
@@ -0,0 +1,21 @@
+package com.example.android.pokedex;
+
+import com.google.gson.annotations.SerializedName;
+
+public class PokeType {
+
+    @SerializedName("name")
+    private String name;
+
+    public PokeType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
